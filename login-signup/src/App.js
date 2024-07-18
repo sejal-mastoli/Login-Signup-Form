426faf97cc24108d85dbe0155fd8916735a2b22e
Sejal Mastoli <mastolisejal7@gmail.com>
2024-07-18 15:18:52 +0530

    Login-signup-form

---
+++ login-signup/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup'; // Adjust the path based on your structure
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
